44da696e35f2fb85e6921b28f61e1717e37ae17c
safi <safiasad12@gmail.com>
2025-02-06 15:21:56 +0530

    [safi] added : model for order details

---
+++ src/model/order.model.ts
@@ -0,0 +1,39 @@
+import { Schema, model } from 'mongoose';
+import { IOrder } from '../interface/order.interface';
+
+const orderSchema = new Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+
+    cart: {
+      totalPrice: {
+        type: Number,
+        default: 0,
+      },
+      totalDiscountPrice: {
+        type: Number,
+        default: 0,
+      },
+      totalQuantity: {
+        type: Number,
+        default: 0,
+      },
+      books: [
+        {
+          bookId: { type: Schema.Types.ObjectId, ref: 'Book' },
+          quantity: { type: Number, required: true },
+        },
+      ],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+orderSchema.index({ userId: 1 }, { background: true });
+
+export default model<IOrder>('Order', orderSchema);
